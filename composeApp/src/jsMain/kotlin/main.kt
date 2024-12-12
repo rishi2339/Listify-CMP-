38c5756ea78f17e1961f516c96716eb0c5904fb6
Rishika Barodia <barodiarishika@gmail.com>
2024-12-12 22:55:14 +0530

    All functionalities working in all four platform.

--- composeApp/src/jsMain/kotlin/main.kt
+++ composeApp/src/jsMain/kotlin/main.kt
@@ -1,4 +1,5 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.CanvasBasedWindow
 import androidx.compose.ui.window.ComposeViewport
 import com.cmp.todo.App
 import com.cmp.todo.di.initKoin
@@ -7,10 +8,16 @@ import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+//    onWasmReady {
+//        val body = document.body ?: return@onWasmReady
+//        initKoin()
+//        ComposeViewport(body) {
+//            App()
+//        }
+//    }
     onWasmReady {
-        val body = document.body ?: return@onWasmReady
         initKoin()
-        ComposeViewport(body) {
+        CanvasBasedWindow("Listify") {
             App()
         }
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,36 +1,37 @@
 rootProject.name = "Listify"
-
+include(":composeApp")
 
 pluginManagement {
     repositories {
-        google {
-            content { 
-              	includeGroupByRegex("com\\.android.*")
-              	includeGroupByRegex("com\\.google.*")
-              	includeGroupByRegex("androidx.*")
-              	includeGroupByRegex("android.*")
-            }
-        }
-        gradlePluginPortal()
+        google()
+//        google {
+//            content {
+//              	includeGroupByRegex("com\\.android.*")
+//              	includeGroupByRegex("com\\.google.*")
+//              	includeGroupByRegex("androidx.*")
+//              	includeGroupByRegex("android.*")
+//            }
+//        }
+//        gradlePluginPortal()
         mavenCentral()
     }
 }
-plugins{
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
-}
+//plugins{
+//    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+//}
 dependencyResolutionManagement {
     repositories {
-        google {
-            content { 
-              	includeGroupByRegex("com\\.android.*")
-              	includeGroupByRegex("com\\.google.*")
-              	includeGroupByRegex("androidx.*")
-              	includeGroupByRegex("android.*")
-            }
-        }
+        google()
+//        google {
+//            content {
+//              	includeGroupByRegex("com\\.android.*")
+//              	includeGroupByRegex("com\\.google.*")
+//              	includeGroupByRegex("androidx.*")
+//              	includeGroupByRegex("android.*")
+//            }
+//        }
         mavenCentral()
     }
 }
-include(":composeApp")
 include(":config:development", ":config:production")
 
